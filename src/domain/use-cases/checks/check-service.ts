71efcd03ce2fe73469e9a8224b12ef6eef9c4b1e
humbert dz <humbertodelac13@gmail.com>
2025-06-16 15:56:38 -0600

    inyeccion de dependencias

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -2,17 +2,30 @@ interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
 
+// types
+type SuccessCallback = () => void;
+type ErrorCallback = (error: string) => void;
+
 export class CheckService implements CheckServiceUseCase {
+  // la inyeccion de dependencias no es más que enviar dependencias a una clase y esto se puede hacer
+  // desde el contructor en ts. en js sería con un factory function
+  // readonly porque solo queremos leer el callback, no queremos modificarlo
+  constructor(
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
+
   public async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
-      console.log(`${url} is ok`);
+      // llamada a la dependencia / callback
+      this.successCallback();
       return true;
     } catch (error) {
-      console.log(error);
+      this.errorCallback(String(error));
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -8,8 +8,13 @@ export class Server {
     CronService.createJob("*/5 * * * * *", () => {
       const date = new Date();
 
-      new CheckService().execute("https://cursos.devtalles.com/");
-      // new CheckService().execute("http://localhost:3000/");
+      // aqui se esta haciendo la inyeccion de dependencias de la manera mas sencilla
+      const url = "https://cursos.devtalles.com/";
+
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute(url);
     });
   }
 }
