497d6f89eccc58daed93b66f8a77e7d3ee896943
humbert dz <humbertodelac13@gmail.com>
2025-06-16 14:43:45 -0600

    CRON Tasks

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,20 @@
+import { CronJob } from "cron";
+
 export class Server {
   static start() {
     console.log("server started...");
+
+    const job = new CronJob(
+      "*/2 * * * * *", // cronTime
+      function () {
+        const date = new Date();
+        console.log(`You will see this message every 2 seconds: ${date}}`);
+      }, // onTick
+      null, // onComplete
+      false, // start
+      "America/Los_Angeles" // timeZone
+    );
+
+    job.start();
   }
 }
