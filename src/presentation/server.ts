3b984b9a0e742b6f1c26ff4632b84b0ee53ffd5e
humbert dz <humbertodelac13@gmail.com>
2025-06-16 15:13:29 -0600

    CheckService - UseCase

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,14 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
 import { CronService } from "./services/cron-service";
 
 export class Server {
   static start() {
     console.log("server started...");
 
-    CronService.createJob(
-      "*/5 * * * * *",
-      () => {
-        const date = new Date();
+    CronService.createJob("*/5 * * * * *", () => {
+      const date = new Date();
 
-        console.log(`se ha ejecutado el cron ${date}`)
-      }
-    );
+      new CheckService().execute("https://cursos.devtalles.com/");
+    });
   }
 }
